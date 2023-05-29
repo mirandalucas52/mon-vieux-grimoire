60e4a23848298116bda84a6099018d3dea1a98e8
Lucas Miranda <mirandalucas52@gmail.com>
2023-05-29 16:08:19 +0200

    sharp added comment

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -3,37 +3,37 @@ const path = require("path");
 const sharp = require("sharp");
 
 const sharpMiddleware = (req, res, next) => {
-    //Vérifie la présence d'un fichier
     if (req.file) {
         const buffer = req.file.buffer;
-        const compressionRate = 50;
+        const compressionRate = 50; // Taux de compression pour les images non webp
 
-        // Si l'image est au format webp, on enregistre sans compresser
-        if (req.file.mimetype === "image/webp") {
-            sharp(buffer).toBuffer((error, buffer) => {
+        sharp(buffer)
+            .webp({
+                quality:
+                    req.file.mimetype === "image/webp" ? 100 : compressionRate, // Utilise un taux de compression élevé pour les images webp, sinon utilise le taux de compression par défaut
+            })
+            .toBuffer((error, buffer) => {
                 if (error) {
                     return next(error);
                 }
-                // Modifie le nom de fichier entrant
+
                 const oldFileName = path
                     .parse(req.file.originalname)
                     .name.split(" ")
-                    .join("_");
+                    .join("_"); // Obtient le nom de fichier d'origine sans espaces
 
-                // Crée le nouveau nom de fichier avec extension webp
-                const timestamp = Date.now();
-                const newFilename = `${oldFileName}_${timestamp}.webp`;
+                const timestamp = Date.now(); // Obtient un horodatage unique
+                const newFilename = `${oldFileName}_${timestamp}.webp`; // Génère un nouveau nom de fichier en ajoutant l'horodatage et l'extension webp
 
-                // Modifie la requête avec le nouveau nom
-                req.file.filename = newFilename;
+                req.file.filename = newFilename; // Met à jour le nom de fichier dans la requête
 
-                // Enregistre l'image dans le dossier "images"
                 const savePath = path.join(
                     __dirname,
                     "..",
                     "images",
                     newFilename
-                );
+                ); // Définit le chemin de sauvegarde du fichier
+
                 fs.writeFile(savePath, buffer, (error) => {
                     if (error) {
                         return next(error);
@@ -41,35 +41,6 @@ const sharpMiddleware = (req, res, next) => {
                     next();
                 });
             });
-        } else {
-            // Si png, jpg, jpeg, on convertit en webp et compresse de 50%
-            sharp(buffer)
-                .webp({ quality: compressionRate })
-                .toBuffer((error, buffer) => {
-                    if (error) {
-                        return next(error);
-                    }
-                    const oldFileName = path
-                        .parse(req.file.originalname)
-                        .name.split(" ")
-                        .join("_");
-                    const timestamp = Date.now();
-                    const newFilename = `${oldFileName}_${timestamp}.webp`;
-                    req.file.filename = newFilename;
-                    const savePath = path.join(
-                        __dirname,
-                        "..",
-                        "images",
-                        newFilename
-                    );
-                    fs.writeFile(savePath, buffer, (error) => {
-                        if (error) {
-                            return next(error);
-                        }
-                        next();
-                    });
-                });
-        }
     } else {
         next();
     }
